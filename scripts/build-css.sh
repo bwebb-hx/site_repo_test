281500228822d2324dfce1869a5f22a27d4d068d
Ben Webb <b.webb@hexabase.com>
2025-08-01 16:40:22 +0900

    fix build-css script

--- scripts/build-css.sh
+++ scripts/build-css.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# IMPORTANT: call this script from the project root; otherwise, it won't find the files correctly.
+# ex: bash scripts/build-css.sh
+
 # convert all scss files to css using sass
 scss_files=$(ls src/scss/*.scss)
 
 for file in $scss_files; do
-  sass $file assets_v2/css/${file%.scss}.css
+  base_name=$(basename $file)
+  npx -y sass "$file" "assets_v2/css/${base_name%.scss}.css" --no-source-map
 done
